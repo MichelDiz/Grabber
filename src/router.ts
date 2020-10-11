a06155d8c10b60f60382f21bcfede2b65ef53546
Michel Diz <michelconrado@outlook.com>
2020-10-11 14:25:57 -0300

    Add routes admin and add basic GraphQL Schema

--- src/router.ts
+++ src/router.ts
@@ -8,6 +8,8 @@ import {
   YouTube,
 } from "./deps.ts";
 
+import {queryGraphQL, mutateGraphQL} from "./helpers.ts"
+
 const router = new Router();
 
 let token = "W4qoPdqjtRG4qBzy2Y";
@@ -22,7 +24,33 @@ let creds: authParams = {
   scope: "https://www.googleapis.com/auth/youtube.readonly",
 };
 
+let addSchema = `mutation {
+  updateGQLSchema(
+    input: { set: { schema: "type Configs { id: ID!@id , API_Token: String, Client_ID: String }"}})
+  {
+    gqlSchema {
+      generatedSchema
+    }
+  }
+}`;
+
+let query = `query test1{
+  getConfigs(id: "0x1"){
+    id
+    API_Token
+    Client_ID
+  }
+  }`;
+
 router
+  .get("/configs", async (ctx: RouterContext)  => {
+    await queryGraphQL(false, query).then(res => ctx.response.body = res.data.getConfigs);
+    ctx.response.type = "application/json";
+  })
+  .get("/autosetup", async (ctx: RouterContext)  => {
+    await mutateGraphQL(true, addSchema)
+    ctx.response.body = "OK";
+  })
 //   .get("/", (ctx: RouterContext) => {
 //     let { access_token } = getQuery(ctx, { mergeParams: true });
 //     console.log(access_token);

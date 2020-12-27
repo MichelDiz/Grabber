0a7374f8e2c794079b4c60cb4d067a56a3a6abe5
Michel Diz <MichelDiz@users.noreply.github.com>
2020-12-27 19:51:18 -0300

    Add Subscription and chat wall

--- googlelogin/src/index.js
+++ googlelogin/src/index.js
@@ -3,9 +3,18 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App/App.js";
 import * as serviceWorker from "./serviceWorker";
+import { WebSocketLink } from "@apollo/client/link/ws";
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
+const link = new WebSocketLink({
+  uri: `ws://localhost:8080/graphql`,
+  options: {
+    reconnect: true,
+  },
+});
+
 const client = new ApolloClient({
+  link,
   uri: "http://localhost:8080/graphql",
   cache: new InMemoryCache(),
 });
--- googlelogin/src/pages/Home/components/HomePage/index.jsx
+++ googlelogin/src/pages/Home/components/HomePage/index.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { useQuery } from "@apollo/client";
+import { useQuery, useSubscription } from "@apollo/client";
 import { loader } from "graphql.macro";
 
 const date0 = new Date();
@@ -16,22 +16,56 @@ const isTokenExpired = () => {
   }
 };
 
-const GET_CONFIGS = loader("../../../../graphql/configs.graphql");
+const GET_CONFIGS = loader("../../../../graphql/subscribeMessages.graphql");
 
 function GetData() {
-  const { loading, error, data } = useQuery(GET_CONFIGS);
+  const { loading, error, data } = useSubscription(GET_CONFIGS);
 
   if (loading) return null;
   if (error) return `Error! ${error}`;
 
   return <main>{JSON.stringify(data)}</main>;
 }
+function ChatWall() {
+  const { loading, error, data } = useSubscription(GET_CONFIGS);
+
+  if (loading) return null;
+  if (error) return `Error! ${error}`;
+
+  return (
+    <>
+      <div className="App">
+        {data &&
+          data.queryMessage.map((msg) => (
+            <ChatMessage key={msg.id} message={msg} />
+          ))}
+      </div>
+    </>
+  );
+}
+function ChatMessage(props) {
+  const { messageText, authorDetails } = props.message;
+  return (
+    <>
+      <div className={`message`}>
+        <div>
+          <img src={authorDetails ? "https://yt3.ggpht.com/ytc/AAUvwnh9_qoajBoWtOjcfUCKCv3gBySz2T_ZLulmvfPpaw=s48-c-k-c0xffffffff-no-rj-mo" : authorDetails[0].profileImageUrl} />
+        </div>
+        <div>
+          <p>{messageText}</p>
+        </div>
+      </div>
+    </>
+  );
+}
+
 export default class index extends Component {
   render() {
     let current_time = Date.now() / 1000;
     // if (current_time > jwt.exp) { /* expired */ }
     return (
       <div>
+        <ChatWall />
         <GetData />
         {JSON.stringify(new Date().getTimezoneOffset())} <br></br>
         {JSON.stringify(date0.getUTCDate())} <br></br>

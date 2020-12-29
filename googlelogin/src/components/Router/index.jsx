b2dac240127ec2c7ac6a3f51caed7ae52273731f
Michel Diz <MichelDiz@users.noreply.github.com>
2020-12-28 21:20:37 -0300

    Added small hack in the static files
    This hack consists of redirecting all paths coming from /index.html to their respective paths inside the application.
    
    I don't understand exactly what the problem is. But as this application is not a "serious" one. I will use this hack. God forgive me.

--- googlelogin/src/components/Router/index.jsx
+++ googlelogin/src/components/Router/index.jsx
@@ -1,35 +1,98 @@
 import React from "react";
-import { BrowserRouter, Route, Switch, Link } from "react-router-dom";
+import { BrowserRouter, Route, Switch, Link, Redirect } from "react-router-dom";
 
 import Home from "../../pages/Home";
 import Login from "../../pages/Login";
 import Settings from "../../pages/Settings";
 
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Button from '@material-ui/core/Button';
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Button from "@material-ui/core/Button";
+
+function CheckSearch(e) {
+  console.log("expr", e.expr);
+  switch (e.expr) {
+    case "?home":
+      return (
+        <Redirect
+          to={{
+            pathname: "/",
+          }}
+        />
+      );
+    case "?settings":
+      return (
+        <Redirect
+          to={{
+            pathname: "/settings",
+          }}
+        />
+      );
+    case "?login":
+      return (
+        <Redirect
+          to={{
+            pathname: "/login",
+          }}
+        />
+      );
+    default:
+      return (
+        <Redirect
+          to={{
+            pathname: "/",
+          }}
+        />
+      );
+  }
+}
 
 const Router = () => {
+  // console.log(this.props.location.pathname);
+
   return (
     <BrowserRouter>
-    <AppBar position="static">
-      <Toolbar>
-      <Button color="inherit" >
-        <Link to="/" className='FakeBTN'>Home</Link>
-      </Button>
-      <Button color="inherit" >
-        <Link to="/login" className='FakeBTN'>Login</Link>
-      </Button>
-      <Button color="inherit" >
-        <Link to="/settings" className='FakeBTN'>Settings</Link>
-      </Button>
-      </Toolbar>
-    </AppBar>
+      {console.log(window.location.pathname)}
+      {console.log(window.location.search)}
+      {console.log(!!window.location.search)}
+      <AppBar position="static">
+        <Toolbar>
+          <Button color="inherit">
+            <Link to="/" className="FakeBTN">
+              Home
+            </Link>
+          </Button>
+          <Button color="inherit">
+            <Link to="/login" className="FakeBTN">
+              Login
+            </Link>
+          </Button>
+          <Button color="inherit">
+            <Link to="/settings" className="FakeBTN">
+              Settings
+            </Link>
+          </Button>
+        </Toolbar>
+      </AppBar>
 
       <Switch>
         <Route exact path="/">
           <Home />
         </Route>
+        <Route
+          exact
+          path="/index.html"
+          render={() =>
+            !window.location.search ? (
+              <Route exact path="/">
+                <Home />
+              </Route>
+            ) : (
+              <CheckSearch expr={window.location.search} />
+            )
+          }
+        />
+
         <Route exact path="/login">
           <Login />
         </Route>
--- src/staticFileMidd.ts
+++ src/staticFileMidd.ts
@@ -4,7 +4,13 @@ export const staticFileMidd = async (ctx: Context, next: Function) => {
   const path = `${Deno.cwd()}/build${ctx.request.url.pathname}`;
 
   if (ctx.request.url.pathname == "/") {
-    ctx.response.redirect("http://localhost:8008/index.html");
+    ctx.response.redirect("http://localhost:8008/index.html?home");
+  }
+  if (ctx.request.url.pathname == "/settings") {
+    ctx.response.redirect("http://localhost:8008/index.html?settings");
+  }
+  if (ctx.request.url.pathname == "/login") {
+    ctx.response.redirect("http://localhost:8008/index.html?login");
   }
 
   if (await fileExists(path)) {
